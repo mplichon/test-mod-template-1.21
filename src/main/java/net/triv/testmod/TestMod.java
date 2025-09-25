55170524d82abcfec178bac68347cb7018fb2af3
mplichon <plichon.matteo@gmail.com>
2025-09-25 20:13:55 +0200

    added pink_garnet_block and raw_pink_garnet_block

--- src/main/java/net/triv/testmod/TestMod.java
+++ src/main/java/net/triv/testmod/TestMod.java
@@ -2,6 +2,7 @@ package net.triv.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.triv.testmod.block.ModBlocks;
 import net.triv.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TestMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
