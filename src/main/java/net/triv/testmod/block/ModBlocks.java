2367bfe6f74d565f7e42f583b52a05e68a95467f
mplichon <plichon.matteo@gmail.com>
2025-09-27 18:22:35 +0200

    added a custom block magic_block

--- src/main/java/net/triv/testmod/block/ModBlocks.java
+++ src/main/java/net/triv/testmod/block/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.triv.testmod.TestMod;
+import net.triv.testmod.block.custom.MagicBlock;
 
 public class ModBlocks {
 
@@ -31,6 +32,9 @@ public class ModBlocks {
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
                     AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
     // Methods
 
