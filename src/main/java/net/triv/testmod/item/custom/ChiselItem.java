20a96eb12838193ea7ba8967900c81ae04311934
mplichon <plichon.matteo@gmail.com>
2025-09-29 20:21:30 +0200

    added tooltips on magic_block, chisel and cauliflower

--- src/main/java/net/triv/testmod/item/custom/ChiselItem.java
+++ src/main/java/net/triv/testmod/item/custom/ChiselItem.java
@@ -2,17 +2,22 @@ package net.triv.testmod.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 import net.triv.testmod.block.ModBlocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -44,5 +49,16 @@ public class ChiselItem extends Item {
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.testmod.chisel.shift_down"));
+        }
+        else {
+            tooltip.add(Text.translatable("tooltip.testmod.chisel"));
+        }
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
