966e31c5fc18479aa8af78a3eaafe5d748bccd4c
mplichon <plichon.matteo@gmail.com>
2025-09-30 19:54:19 +0200

    added ModTags and mod tag transformable_items for magic_block

--- src/main/java/net/triv/testmod/block/custom/MagicBlock.java
+++ src/main/java/net/triv/testmod/block/custom/MagicBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.triv.testmod.item.ModItems;
+import net.triv.testmod.util.ModTags;
 
 import java.util.List;
 
@@ -35,11 +36,15 @@ public class MagicBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
-            if (itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if (isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
