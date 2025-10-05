c0f6e6ffd61472a48bbdf8b72b4ed0a8fb0dcd97
mplichon <plichon.matteo@gmail.com>
2025-10-05 17:41:47 +0200

    added pink_garnet_tools

--- src/main/java/net/triv/testmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/triv/testmod/datagen/ModBlockTagProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
 import net.triv.testmod.block.ModBlocks;
+import net.triv.testmod.util.ModTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -29,12 +30,15 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
         getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
                 .add(ModBlocks.PINK_GARNET_DEEPSLATE_ORE)
-                .add(ModBlocks.PINK_GARNET_BLOCK);
+                .add(ModBlocks.PINK_GARNET_BLOCK)
+                .add(ModBlocks.MAGIC_BLOCK);
 
         getOrCreateTagBuilder(BlockTags.FENCES).add(ModBlocks.PINK_GARNET_FENCE);
-
         getOrCreateTagBuilder(BlockTags.FENCE_GATES).add(ModBlocks.PINK_GARNET_FENCE_GATE);
-
         getOrCreateTagBuilder(BlockTags.WALLS).add(ModBlocks.PINK_GARNET_WALL);
+
+        getOrCreateTagBuilder(ModTags.Blocks.NEEDS_PINK_GARNET_TOOL)
+                .add(ModBlocks.MAGIC_BLOCK)
+                .addTag(BlockTags.NEEDS_IRON_TOOL);
     }
 }
