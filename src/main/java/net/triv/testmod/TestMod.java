337334f168301e27bb3cc986fe2b8459ab3fadce
mplichon <plichon.matteo@gmail.com>
2025-10-08 20:43:30 +0200

    added an event when the player attacks a sheep with an end rod

--- src/main/java/net/triv/testmod/TestMod.java
+++ src/main/java/net/triv/testmod/TestMod.java
@@ -2,8 +2,15 @@ package net.triv.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.triv.testmod.block.ModBlocks;
 import net.triv.testmod.component.ModDataComponentTypes;
 import net.triv.testmod.item.ModItemGroups;
@@ -27,5 +34,17 @@ public class TestMod implements ModInitializer {
         FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
 
         PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+        AttackEntityCallback.EVENT.register((player, world, hand, entity, entityHitResult) -> {
+            if (entity instanceof SheepEntity sheepEntity && !world.isClient()) {
+                if (player.getMainHandStack().getItem() == Items.END_ROD) {
+                    player.sendMessage(Text.literal("The Player just hit a sheep with an END ROD! YOU SICK FRICK!"));
+                    player.getMainHandStack().decrement(1);
+                    sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 600, 6));
+                }
+
+                return ActionResult.PASS;
+            }
+            return ActionResult.PASS;
+        });
 	}
 }
